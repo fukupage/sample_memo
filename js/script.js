5dbb74e50e40b0b59d14da762c51ea16629d9bd9
fukupage <4033990+fukupage@users.noreply.github.com>
2020-05-25 10:36:01 +0900

    LocalStorageに格納した内容が再表示されないけど…

--- js/script.js
+++ js/script.js
@@ -3,7 +3,36 @@
 const addForm = document.getElementById('form');
 const list = document.getElementById('list');
 const submit = document.getElementById('submit');
-const titleActive = document.getElementsByTagName('memoTitle');
+const search = document.querySelector('.search input');
+
+(function(){
+    for(var key in localStorage){
+        var html = localStorage.getItem(key);
+        if(html){
+            list.inner += localStorage.getItem(key);
+            console.log(key + 'flag1');
+            console.log(html + 'flag1');
+        }
+    }
+})();
+
+const saveTaskToLocalStorage = (title,comment,html) =>{
+    if(html){
+        localStorage.setItem(title,comment,html);
+        console.log(title);
+        console.log(comment);
+        console.log(html);
+    return;
+    }
+    return;
+}
+
+const deleteTaskFromLocalStorage = (title,comment) =>{
+    localStorage.removeItem(title,comment);
+    console.log(key + 'flag1');
+    console.log(html + 'flag1');
+return;
+}
 
 const createTodoList = (title, comment) => {
     const html = `
@@ -14,6 +43,7 @@ const createTodoList = (title, comment) => {
     </li>
     `;
     list.innerHTML += html;
+    saveTaskToLocalStorage(title,comment,html);
 }
 
 submit.addEventListener('click', e => {
@@ -31,6 +61,35 @@ list.addEventListener('click', e => {
         e.target.parentElement.remove();
     }
 });
+
+const filterTitle = title => {
+    console.log('flag1');
+    Array.from(list.children)
+    .filter((todo) => !todo.textContent.toLowerCase().includes(title))
+    .forEach((todo) => todo.classList.add('filterd'));
+
+    Array.from(list.children)
+    .filter((todo) => todo.textContent.toLowerCase().includes(title))
+    .forEach((todo) => todo.classList.remove('filltered'));
+};
+
+const filterComment = comment => {
+    console.log('flag1');
+    Array.from(list.children)
+    .filter((todo) => !todo.textContent.toLowerCase().includes(comment))
+    .forEach((todo) => todo.classList.add('filterd'));
+
+    Array.from(list.children)
+    .filter((todo) => todo.textContent.toLowerCase().includes(comment))
+    .forEach((todo) => todo.classList.remove('filltered'));
+};
+
+search.addEventListener('keyup',() => {
+    const text = search.value.trim().toLowerCase();
+    filterTitle(text);
+    filterComment(text);
+});
+
 
 
 // const addTask = document.querySelector('.add');
